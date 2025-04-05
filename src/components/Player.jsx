1ade4f8faba3a42eaa0f3dd6249465137256f20f
Evgeniy Akulenko <akulenko.contact@gmail.com>
2025-04-05 15:42:43 +0300

    fix(Player.jsx): add editing feature and save button

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,26 @@
-function Player({playerName, symbol}) {
+import {useState} from "react";
+
+function Player({ name, symbol}) {
+    const [isEditing, setEditing] = useState(false);
+    function handleEditClick() {
+        setEditing((prev) => !prev);
+    }
+
+    let playerName = <span className='player-name'>{name}</span>;
+    let btnCaption = 'Edit';
+
+    if (isEditing) {
+        playerName = <input type='text' placeholder='Введите имя игрока' required></input>
+        btnCaption = 'Save';
+    }
+
     return (
         <li>
             <span className='player'>
-                <span className='player-name'>{playerName}</span>
+                {playerName}
                 <span className='player-symbol'>{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={handleEditClick}>{btnCaption}</button>
         </li>
     );
 }
