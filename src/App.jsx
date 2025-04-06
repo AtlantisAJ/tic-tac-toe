278e089b40ca176ce63d029816c240ce4957ef4b
Evgeniy Akulenko <akulenko.contact@gmail.com>
2025-04-06 15:31:29 +0300

    docs(winning-combinations.js): Define WINNING_COMBINATIONS and import it into App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Player from './components/Player.jsx';
 import GameBoard from "./components/GameBoard.jsx";
 import Log from "./components/Log.jsx";
 
+import {WINNING_COMBINATIONS} from "./winning-combinations.js";
+
 function deriveActivePlayer(gameTurns) {
     let currentPlayer = 'X';
 
@@ -15,19 +17,13 @@ function deriveActivePlayer(gameTurns) {
 
 function App() {
     const [gameTurns, setGameTurns] = useState([]);
-    // const [activePlayer, setActivePlayer] = useState('X');
+    console.log(gameTurns);
 
     const activePlayer = deriveActivePlayer(gameTurns)
 
     function handleSelectSquare(rowIndex, colIndex) {
-        // setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X')
         setGameTurns((prevTurns) => {
-            // let currentPlayer = 'X';
             const currentPlayer = deriveActivePlayer(prevTurns);
-
-            // if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
-            //     currentPlayer = 'O';
-            // }
 
             const updatedTurns = [{square: {row: rowIndex, col: colIndex}, player: currentPlayer}, ...prevTurns];
         return updatedTurns
