b2d9c802801bc8191eac375cfc12232d26706016
Evgeniy Akulenko <akulenko.contact@gmail.com>
2025-04-06 14:10:43 +0300

    fix(components/GameBoard.jsx): Update GameBoard component to use "onSelectSquare" hook and pass turns array.

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,4 +1,3 @@
-import {useState} from "react";
 
 const initialGameBoard = [
     [null, null, null],
@@ -6,24 +5,34 @@ const initialGameBoard = [
     [null, null, null]
 ]
 
-function GameBoard({ onSelectSquare, activePlayerSymbol }) {
-    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+function GameBoard({ onSelectSquare, turns }) {
+    let gameBoard = initialGameBoard;
 
-    function handleSelectSquare(rowIndex, colIndex) {
-        setGameBoard((prevGameBoard) => {
-            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-            updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-            return updatedBoard;
-        });
+    for (const turn of turns) {
+        const { square, player } = turn;
+        const { row, col} = square;
 
-        onSelectSquare()
+        gameBoard[row][col] = player;
     }
+
+
+    // const [gameBoard, setGameBoard] = useState(initialGameBoard);
+    //
+    // function handleSelectSquare(rowIndex, colIndex) {
+    //     setGameBoard((prevGameBoard) => {
+    //         const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+    //         updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+    //         return updatedBoard;
+    //     });
+    //
+    //     onSelectSquare()
+    // }
 
     return (
         <ol id='game-board'>
             {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
                 <ol>
-                    {row.map((playerSymbol, colIndex) => <li key={colIndex}><button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button></li>)}
+                    {row.map((playerSymbol, colIndex) => <li key={colIndex}><button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button></li>)}
                 </ol>
             </li>)}
         </ol>
