9952f97466e9921c467eae86d571c2b565732724
Evgeniy Akulenko <akulenko.contact@gmail.com>
2025-04-06 14:17:09 +0300

    fix(components/GameBoard.jsx): Update GameBoard component to use "onSelectSquare" hook and pass turns array.

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ function App() {
 
             <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns}/>
         </div>
-        <Log/>
+        <Log turns={gameTurns}/>
     </main>
   )
 }
--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,8 +1,10 @@
 
 
-function Log(props) {
+function Log({turns}) {
     return (
-        <ol id='log'></ol>
+        <ol id='log'>
+            {turns.map(turn => <li key={`${turn.square.row}${turn.square.col}`}>{turn.player} selected {turn.square.row},{turn.square.col}</li>)}
+        </ol>
     );
 }
 
