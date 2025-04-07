7885c2e0854a6b1b31be3c077654ae7ce6e23753
Evgeniy Akulenko <akulenko.contact@gmail.com>
2025-04-07 20:51:53 +0300

    refactor(utils): Simplify game logic utilities by extracting functions into a single module.

--- src/App.jsx
+++ src/App.jsx
@@ -5,53 +5,13 @@ import GameBoard from "./components/GameBoard.jsx";
 import Log from "./components/Log.jsx";
 import GameOver from "./components/GameOver.jsx";
 
-import {WINNING_COMBINATIONS} from "./constants/winning-combinations.js";
-import {INITIAL_GAME_BOARD} from "./constants/game-board.js";
+import {
+    deriveActivePlayer,
+    deriveGameBoard,
+    deriveWinner
+} from "./utils/game-utils.js";
+
 import {PLAYERS} from "./constants/players.js";
-
-
-function deriveActivePlayer(gameTurns) {
-    let currentPlayer = 'X';
-
-    if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
-        currentPlayer = 'O';
-    }
-    return  currentPlayer;
-};
-
-function deriveGameBoard(gameTurns) {
-    let gameBoard = [...INITIAL_GAME_BOARD.map(array => [...array])];
-
-    for (const turn of gameTurns) {
-        const { square, player } = turn;
-        const { row, col } = square;
-
-        gameBoard[row][col] = player;
-    }
-    return gameBoard;
-}
-
-function deriveWinner(gameBoard, players) {
-    let winner;
-
-    for (const combination of WINNING_COMBINATIONS) {
-        const firstSquareSymbol =
-            gameBoard[combination[0].row][combination[0].column];
-        const secondSquareSymbol =
-            gameBoard[combination[1].row][combination[1].column];
-        const thirdSquareSymbol =
-            gameBoard[combination[2].row][combination[2].column];
-
-        if (
-            firstSquareSymbol &&
-            firstSquareSymbol === secondSquareSymbol &&
-            firstSquareSymbol === thirdSquareSymbol)
-        {
-            winner = players[firstSquareSymbol]
-        }
-    }
-    return winner;
-}
 
 function App() {
     const [players, setPlayers] = useState(PLAYERS);
