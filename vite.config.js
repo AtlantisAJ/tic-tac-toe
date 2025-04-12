c75628f96563fa40647e6b9b2b34f18dbd8dd5da
Evgeniy Akulenko <akulenko.contact@gmail.com>
2025-04-12 13:08:46 +0300

    Deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/atlantisaj.github.io/', // Замените на имя вашего репозитория (например, 'tic-tac-toe')
+  build: {
+    outDir: 'dist', // Убедитесь, что сборка идёт в папку dist
+  },
+});
